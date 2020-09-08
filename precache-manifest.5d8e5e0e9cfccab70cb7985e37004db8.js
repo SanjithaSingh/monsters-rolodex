eb8646e08f0bd3c02528586c220938535432a7c6
Sanjitha Singh <sanjitha.singh@gmail.com>
2020-09-08 12:44:55 +0530

    Updates

---
+++ precache-manifest.5d8e5e0e9cfccab70cb7985e37004db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadd3b38a2ee798100cf823cb8cdaa2e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3c2f50b4a903b6a6baa9",
+    "url": "/monsters-rolodex/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2f50b4a903b6a6baa9",
+    "url": "/monsters-rolodex/static/js/main.dae8979b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
